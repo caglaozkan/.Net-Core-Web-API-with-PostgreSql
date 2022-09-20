e18bc1940eeb16fb7235901a6d13c27d9229b36a
Çağla Özkan İnan <caglaozkan0109@gmail.com>
2022-09-20 17:31:01 +0300

    Update CustomResponseDto.cs

--- NLayer.Core/DTOs/CustomResponseDto.cs
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -7,30 +7,29 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.DTOs
 {
-    public class CustomResponseDto<T>  // generic T tipinde
+    public class CustomResponseDto<T>  
     {
         public T Data { get; set; }
 
-        [JsonIgnore]   // status kod clientlara görünmicek.zaten postman bu kodu döner.
+        [JsonIgnore]   
         public int StatusCode { get; set; }
 
         public List<String> Errors { get; set; }
 
-        // new ile oluşturmak yerine burda static olarak yazıyorum.
-        // aşağıdaki kodlama stili static factory method dp olarak geçer.
+       
         public static CustomResponseDto<T> Success(int statusCode,T data)
         {
             return new CustomResponseDto<T> { StatusCode = statusCode, Data = data};
         }
-        public static CustomResponseDto<T> Success(int statusCode)   // her zaman geriye data dönmeyiz.sadece durum kodu döneriz.
+        public static CustomResponseDto<T> Success(int statusCode)   
         {
             return new CustomResponseDto<T> { StatusCode = statusCode };
         }
-        public static CustomResponseDto<T> Fail(int statusCode , List<string> errors)   // her zaman geriye data dönmeyiz.sadece durum kodu döneriz.
+        public static CustomResponseDto<T> Fail(int statusCode , List<string> errors)  
         {
             return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors  };
         }
-        public static CustomResponseDto<T> Fail(int statusCode, string error)   // her zaman geriye data dönmeyiz.sadece durum kodu döneriz.
+        public static CustomResponseDto<T> Fail(int statusCode, string error)   
         {
             return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
         }
